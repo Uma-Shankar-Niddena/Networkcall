559c1543166744348926f9cf906310ab77206e7c
Uma-Shankar-Niddena <155518393+Uma-Shankar-Niddena@users.noreply.github.com>
2024-04-17 20:47:52 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const path = require('path')
+
+const databasepath = path.join(__dirname, 'cricketMatchDetails.db')
+let db = null
+const initializedbserver = async () => {
+  try {
+    db = await open({
+      filename: databasepath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server running ... enjoy pandagoww')
+    })
+  } catch (e) {
+    console.log(`something went wron ${e.message}`)
+  }
+}
+initializedbserver()
+
+///GEt listof players in the playertable
+app.get('/players/', async (request, response) => {
+  const querytogetallaplayer = `SELECT player_id as playerId, player_name as playerName from player_details`
+  const resfromdb = await db.all(querytogetallaplayer)
+  response.send(resfromdb)
+})
+
+///GEt specific player with playerid
+app.get('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const querygetSpecific = `SELECT player_id AS playerId, player_name AS playerName FROM player_details WHERE player_id=${playerId}`
+  const resfromdb = await db.all(querygetSpecific)
+  response.send(resfromdb)
+})
+
+///PUT specific player by playerid
+app.put('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const playerDetails = request.body
+  const {playerName} = playerDetails
+  const queryforupdatePlayer = `UPDATE player_details SET player_name="${playerName}" WHERE player_id="${playerId}"`
+  const resfromdb = await db.run(queryforupdatePlayer)
+  response.send('Player Details Updated')
+})
+
+/// GEt match details
+app.get('/matches/:matchId/', async (request, response) => {
+  const {matchId} = request.params
+  const queryformatchdetails = `SELECT match_id AS matchId, match AS match, year AS year FROM match_details WHERE match_id =${matchId}`
+  const resfromdb = await db.all(queryformatchdetails)
+  response.send(resfromdb)
+})
+
+/// GET allthe matches of a player
+app.get('/players/:playerId/matches/', async (request, response) => {
+  const {playerId} = request.params
+  const queryforplayer = `SELECT match_details.match_id AS matchId, match_details.match AS match, match_details.year AS year FROM match_details NATURAL JOIN player_details WHERE player_details.player_id=${playerId}`
+
+  const resfromdb = await db.all(queryforplayer)
+  response.send(resfromdb)
+})
+
+/// GET list of players of a specific match
+
+app.get('/matches/:matchId/players/', async (request, response) => {
+  const {matchId} = request.params
+  const querytogetplayer = `SELECT
+	      player_details.player_id AS playerId,
+	      player_details.player_name AS playerName
+	    FROM player_match_score NATURAL JOIN player_details
+        WHERE match_id=${matchId};`
+  const responsedb2 = await db.all(querytogetplayer)
+  response.send(responsedb2)
+})
+
+/// GET total fours scores sixes
+app.get('/players/:playerId/playerScores/', async (request, response) => {
+  const {playerId} = request.params
+  const queryforplayernames = `SELECT
+    player_details.player_id AS playerId,
+    player_details.player_name AS playerName,
+    SUM(player_match_score.score) AS totalScore,
+    SUM(fours) AS totalFours,
+    SUM(sixes) AS totalSixes FROM 
+    player_details INNER JOIN player_match_score ON
+    player_details.player_id = player_match_score.player_id
+    WHERE player_details.player_id = ${playerId};`
+  const resfromdb = await db.get(queryforplayernames)
+  response.send(resfromdb)
+})
+module.exports = app
